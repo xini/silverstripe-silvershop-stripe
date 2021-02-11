ae8269896967eb3a41bfbd1d5ff3110014d07ea1
Bettina <b.broethaler@pixelpoems.com>
2021-02-11 17:12:44 +0100

    first try to get Payment_Intents working

--- src/Checkout/Components/StripeOnsitePayment.php
+++ src/Checkout/Components/StripeOnsitePayment.php
@@ -3,6 +3,10 @@
 namespace Innoweb\SilvershopStripe\Checkout\Components;
 
 use Innoweb\SilvershopStripe\Forms\StripeField;
+use Omnipay\Common\AbstractGateway;
+use Omnipay\Stripe\AbstractGateway as StripeAbstractGateway;
+use Omnipay\Stripe\Gateway;
+use Omnipay\Stripe\PaymentIntentsGateway;
 use SilverShop\Checkout\Checkout;
 use SilverShop\Checkout\Component\OnsitePayment;
 use SilverShop\Model\Order;
@@ -34,17 +38,19 @@ class StripeOnsitePayment extends OnsitePayment
     use Injectable;
     use Extensible;
     use Configurable;
-    
+
     /** @var bool - if for some reason the gateway is not actually stripe, fall back to OnsitePayment */
     protected $isStripe;
-    
-    /** @var \Omnipay\Common\AbstractGateway|\Omnipay\Stripe\Gateway */
+
+    protected $isPaymentIntents;
+
+    /** @var AbstractGateway|Gateway */
     protected $gateway;
-    
+
     /**
      * @param Order $order
      *
-     * @return \Omnipay\Common\AbstractGateway|\Omnipay\Stripe\Gateway
+     * @return AbstractGateway|Gateway
      */
     protected function getGateway($order)
     {
@@ -56,23 +62,23 @@ class StripeOnsitePayment extends OnsitePayment
             );
             $service = PurchaseService::create($tempPayment);
             $this->gateway = $service->oGateway();
-            $this->isStripe = ($this->gateway instanceof \Omnipay\Stripe\Gateway);
+            $this->isStripe = ($this->gateway instanceof StripeAbstractGateway);
         }
-        
+
         return $this->gateway;
     }
-    
+
     /**
-     * @param \Omnipay\Common\AbstractGateway|\Omnipay\Stripe\Gateway $gateway
+     * @param AbstractGateway|Gateway $gateway
      * @return $this
      */
     public function setGateway($gateway)
     {
         $this->gateway = $gateway;
-        $this->isStripe = ($this->gateway instanceof \Omnipay\Stripe\Gateway);
+        $this->isStripe = ($this->gateway instanceof StripeAbstractGateway);
         return $this;
     }
-    
+
     /**
      * Get form fields for manipulating the current order,
      * according to the responsibility of this component.
@@ -87,7 +93,7 @@ class StripeOnsitePayment extends OnsitePayment
         if (!$this->isStripe) {
             return parent::getFormFields($order);
         }
-        
+
         // Generate the standard set of fields and allow it to be customised
         $fields = FieldList::create(
             [
@@ -101,11 +107,12 @@ class StripeOnsitePayment extends OnsitePayment
             $fields->unshift($existingCardField);
             $stripeField->setTitle(_t(static::class.'.NewCreditCard', 'New credit or debit card'));
         }
-        
+
         $this->extend('updateFormFields', $fields);
-        
+
         // Generate a basic config and allow it to be customised
-        $stripeConfig = Config::inst()->get(GatewayInfo::class, 'Stripe');
+        $configName = $this->gateway instanceof PaymentIntentsGateway ? 'Stripe_PaymentIntents' : 'Stripe';
+        $stripeConfig = Config::inst()->get(GatewayInfo::class, $configName);
         $jsConfig = [
             'formID'        => 'PaymentForm_PaymentForm',
             'stripeField'   => 'PaymentForm_PaymentForm_' . $stripeField->getName(),
@@ -115,19 +122,19 @@ class StripeOnsitePayment extends OnsitePayment
                                 : '',
         ];
         $this->extend('updateStripeConfig', $jsConfig);
-        
+
         if (empty($jsConfig['key'])) {
             user_error('Publishable key was not set. Should be in GatewayInfo.Stripe.parameters.publishableKey.');
         }
-        
+
         // Finally, add the javascript to the page
         Requirements::customScript("window.StripeConfig = " . json_encode($jsConfig), 'StripeJS');
         Requirements::javascript('https://js.stripe.com/v3/');
         Requirements::javascript('innoweb/silverstripe-silvershop-stripe:javascript/checkout.js');
-        
+
         return $fields;
     }
-    
+
     /**
      * @param Member $member
      * @return bool
@@ -140,7 +147,7 @@ class StripeOnsitePayment extends OnsitePayment
         if (!$member) $member = Security::getCurrentUser();
         return $member && $member->CreditCards()->exists();
     }
-    
+
     /**
      * Allow choosing from an existing credit cards
      * @return FormField|null field
@@ -194,7 +201,7 @@ class StripeOnsitePayment extends OnsitePayment
         if (!$this->isStripe) {
             return parent::validateData($order, $data);
         } else {
-            
+
             // If existing card selected, check that it exists in $member->CreditCards
             $existingID = !empty($data['SavedCreditCardID']) ? (int)$data['SavedCreditCardID'] : 0;
             if ($existingID) {
@@ -204,7 +211,7 @@ class StripeOnsitePayment extends OnsitePayment
                     throw new ValidationException($result);
                 }
             }
-            
+
             // NOTE: Stripe will validate clientside and if for some reason that falls through
             // it will fail on payment and give an error then. It would be a lot of work to get
             // the token to be namespaced so it could be passed here and there would be no point.
--- src/Checkout/StripeOrderProcessor.php
+++ src/Checkout/StripeOrderProcessor.php
@@ -14,7 +14,7 @@ use Innoweb\SilvershopStripe\Checkout\Components\StripeOnsitePayment;
 
 class StripeOrderProcessor extends OrderProcessor
 {
-    
+
     /**
      * Handle payment with Stripe's stored customer and credit card details
      *
@@ -31,24 +31,24 @@ class StripeOrderProcessor extends OrderProcessor
     public function makePayment($gateway, $gatewaydata = array(), $successUrl = null, $cancelUrl = null)
     {
         // only do this for Stripe
-        if ($gateway != 'Stripe') {
+        if (!in_array($gateway, ['Stripe', 'Stripe_PaymentIntents'])) {
             return parent::makePayment($gateway, $gatewaydata, $successUrl, $cancelUrl);
         }
-        
+
         //create payment
         $payment = $this->createPayment($gateway);
         if (!$payment) {
             //errors have been stored.
             return null;
         }
-        
+
         $payment->setSuccessUrl($successUrl ? $successUrl : $this->getReturnUrl());
-        
+
         // Explicitly set the cancel URL
         if ($cancelUrl) {
             $payment->setFailureUrl($cancelUrl);
         }
-        
+
         // Create a payment service, by using the Service Factory. This will automatically choose an
         // AuthorizeService or PurchaseService, depending on Gateway configuration.
         // Set the user-facing success URL for redirects
@@ -57,12 +57,12 @@ class StripeOrderProcessor extends OrderProcessor
          */
         $factory = ServiceFactory::create();
         $service = $factory->getService($payment, ServiceFactory::INTENT_PAYMENT);
-        
+
         $gatewaydata = $this->getGatewayData($gatewaydata);
-        
+
         // save stripe customer and credit card, update data
         $gatewaydata = $this->saveCustomerAndCard($service, $payment, $gatewaydata);
-        
+
         // Initiate payment, get the result back
         try {
             $serviceResponse = $service->initiate($gatewaydata);
@@ -71,7 +71,7 @@ class StripeOrderProcessor extends OrderProcessor
             $this->error($ex->getMessage());
             return null;
         }
-        
+
         // Check if the service response itself contains an error
         if ($serviceResponse->isError()) {
             if ($opResponse = $serviceResponse->getOmnipayResponse()) {
@@ -80,13 +80,13 @@ class StripeOrderProcessor extends OrderProcessor
                 $this->error('An unspecified payment error occurred. Please check the payment messages.');
             }
         }
-        
+
         // For an OFFSITE payment, serviceResponse will now contain a redirect
         // For an ONSITE payment, ShopPayment::onCaptured will have been called, which will have called completePayment
-        
+
         return $serviceResponse;
     }
-    
+
     /**
      * store customer and credit card reference
      *
@@ -98,19 +98,19 @@ class StripeOrderProcessor extends OrderProcessor
     protected function saveCustomerAndCard($service, $payment, $gatewaydata)
     {
         if ($payment) {
-            
+
             // only do this for Stripe
             if ($payment->Gateway != 'Stripe') {
                 return;
             }
-            
+
             // update member and credit card
             $member = Security::getCurrentUser();
             if (!$member) {
                 $member = $this->order->Member();
             }
             if ($member && $member->exists()) {
-                
+
                 // create new customer object in Stripe and store reference
                 if (!$member->StripeCustomerReference) {
                     $stripeData = [
@@ -126,11 +126,11 @@ class StripeOrderProcessor extends OrderProcessor
                         $this->error($response->getMessage());
                     }
                 }
-                
+
                 // create new card if new one submitted
                 if ($member->StripeCustomerReference) {
                     if (empty($gatewaydata['SavedCreditCardID']) || $gatewaydata['SavedCreditCardID'] == 'newcard') {
-                        
+
                         $request = $service->oGateway()->createCard([
                             'cardReference' => isset($gatewaydata['token']) ? $gatewaydata['token'] : '',
                             'customerReference' => $member->StripeCustomerReference,
@@ -159,21 +159,21 @@ class StripeOrderProcessor extends OrderProcessor
                         } else {
                             $this->error($response->getMessage());
                         }
-                        
+
                     } else {
                         // this will have been validated in OnsitePaymentCheckoutComponent
                         $payment->SavedCreditCardID = $gatewaydata['SavedCreditCardID'];
                         $payment->write();
                     }
                 }
-                
+
                 // update stripe data, replacing token with customer/card
                 if ($member->StripeCustomerReference) {
-                    
+
                     // remove token already used for customer creation, replace with customer reference
                     unset($gatewaydata['token']);
                     $gatewaydata['customerReference'] = $member->StripeCustomerReference;
-                    
+
                     // add credit card reference for this payment if available
                     if ($card = $payment->SavedCreditCard()) {
                         $gatewaydata['cardReference'] = $card->CardReference;
@@ -181,14 +181,14 @@ class StripeOrderProcessor extends OrderProcessor
                 }
             }
         }
-        
+
         return $gatewaydata;
     }
-    
+
     protected function getGatewayData($customData)
     {
         $data = parent::getGatewayData($customData);
-        
+
         // add description
         $data['description'] = $data['firstName'] . ' ' . $data['lastName'] . ' | ';
         if ($this->order->BillingAddress()->Company) {
@@ -197,8 +197,8 @@ class StripeOrderProcessor extends OrderProcessor
         $data['description'] .= $data['email'] . ' | ' . $data['transactionId'] . ' ';
 
         $this->order->extend('updateGetGatewayData', $data);
-        
+
         return $data;
     }
-    
+
 }
