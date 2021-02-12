befe2f25d37a245e06cd5d5e5e7e5211aff5f7f6
Werner M. Krauß <werner.krauss@netwerkstatt.at>
2021-02-12 22:15:25 +0100

    Payment Intents integration

--- src/Checkout/Components/StripeOnsitePayment.php
+++ src/Checkout/Components/StripeOnsitePayment.php
@@ -3,6 +3,7 @@
 namespace Innoweb\SilvershopStripe\Checkout\Components;
 
 use Innoweb\SilvershopStripe\Forms\StripeField;
+use Omnipay\Stripe\PaymentIntentsGateway;
 use SilverShop\Checkout\Checkout;
 use SilverShop\Checkout\Component\OnsitePayment;
 use SilverShop\Model\Order;
@@ -34,17 +35,19 @@ class StripeOnsitePayment extends OnsitePayment
     use Injectable;
     use Extensible;
     use Configurable;
-    
+
     /** @var bool - if for some reason the gateway is not actually stripe, fall back to OnsitePayment */
     protected $isStripe;
-    
-    /** @var \Omnipay\Common\AbstractGateway|\Omnipay\Stripe\Gateway */
+    /** @var bool - do we use Payment Intents? */
+    protected $isPaymentIntent;
+
+    /** @var \Omnipay\Common\AbstractGateway|\Omnipay\Stripe\AbstractGateway */
     protected $gateway;
-    
+
     /**
      * @param Order $order
      *
-     * @return \Omnipay\Common\AbstractGateway|\Omnipay\Stripe\Gateway
+     * @return \Omnipay\Common\AbstractGateway|\Omnipay\Stripe\AbstractGateway
      */
     protected function getGateway($order)
     {
@@ -56,12 +59,13 @@ class StripeOnsitePayment extends OnsitePayment
             );
             $service = PurchaseService::create($tempPayment);
             $this->gateway = $service->oGateway();
-            $this->isStripe = ($this->gateway instanceof \Omnipay\Stripe\Gateway);
+            $this->isStripe = ($this->gateway instanceof \Omnipay\Stripe\AbstractGateway);
+            $this->isPaymentIntent = $this->gateway instanceof PaymentIntentsGateway;
         }
-        
+
         return $this->gateway;
     }
-    
+
     /**
      * @param \Omnipay\Common\AbstractGateway|\Omnipay\Stripe\Gateway $gateway
      * @return $this
@@ -69,10 +73,11 @@ class StripeOnsitePayment extends OnsitePayment
     public function setGateway($gateway)
     {
         $this->gateway = $gateway;
-        $this->isStripe = ($this->gateway instanceof \Omnipay\Stripe\Gateway);
+        $this->isStripe = ($this->gateway instanceof \Omnipay\Stripe\AbstractGateway);
+        $this->isPaymentIntent = $this->gateway instanceof PaymentIntentsGateway;
         return $this;
     }
-    
+
     /**
      * Get form fields for manipulating the current order,
      * according to the responsibility of this component.
@@ -87,7 +92,7 @@ class StripeOnsitePayment extends OnsitePayment
         if (!$this->isStripe) {
             return parent::getFormFields($order);
         }
-        
+
         // Generate the standard set of fields and allow it to be customised
         $fields = FieldList::create(
             [
@@ -101,33 +106,41 @@ class StripeOnsitePayment extends OnsitePayment
             $fields->unshift($existingCardField);
             $stripeField->setTitle(_t(static::class.'.NewCreditCard', 'New credit or debit card'));
         }
-        
+
         $this->extend('updateFormFields', $fields);
-        
+
         // Generate a basic config and allow it to be customised
-        $stripeConfig = Config::inst()->get(GatewayInfo::class, 'Stripe');
+        $configName = $this->isPaymentIntent ? 'Stripe_PaymentIntents' : 'Stripe';
+        $stripeConfig = Config::inst()->get(GatewayInfo::class, $configName);
         $jsConfig = [
             'formID'        => 'PaymentForm_PaymentForm',
             'stripeField'   => 'PaymentForm_PaymentForm_' . $stripeField->getName(),
             'tokenField'    => 'PaymentForm_PaymentForm_' . $tokenField->getName(),
+            'submitButton'  => 'PaymentForm_PaymentForm_action_submitpayment',
             'key'           => isset($stripeConfig['parameters']) && isset($stripeConfig['parameters']['publishableKey'])
                                 ? $stripeConfig['parameters']['publishableKey']
                                 : '',
         ];
         $this->extend('updateStripeConfig', $jsConfig);
-        
+
         if (empty($jsConfig['key'])) {
             user_error('Publishable key was not set. Should be in GatewayInfo.Stripe.parameters.publishableKey.');
         }
-        
+
         // Finally, add the javascript to the page
         Requirements::customScript("window.StripeConfig = " . json_encode($jsConfig), 'StripeJS');
         Requirements::javascript('https://js.stripe.com/v3/');
-        Requirements::javascript('innoweb/silverstripe-silvershop-stripe:javascript/checkout.js');
-        
+        if($this->isPaymentIntent) {
+            Requirements::javascript('innoweb/silverstripe-silvershop-stripe:javascript/checkout_paymentintents.js');
+        }
+        if (!$this->isPaymentIntent) {
+            Requirements::javascript('innoweb/silverstripe-silvershop-stripe:javascript/checkout.js');
+        }
+
+
         return $fields;
     }
-    
+
     /**
      * @param Member $member
      * @return bool
@@ -140,7 +153,7 @@ class StripeOnsitePayment extends OnsitePayment
         if (!$member) $member = Security::getCurrentUser();
         return $member && $member->CreditCards()->exists();
     }
-    
+
     /**
      * Allow choosing from an existing credit cards
      * @return FormField|null field
@@ -194,7 +207,7 @@ class StripeOnsitePayment extends OnsitePayment
         if (!$this->isStripe) {
             return parent::validateData($order, $data);
         } else {
-            
+
             // If existing card selected, check that it exists in $member->CreditCards
             $existingID = !empty($data['SavedCreditCardID']) ? (int)$data['SavedCreditCardID'] : 0;
             if ($existingID) {
@@ -204,7 +217,7 @@ class StripeOnsitePayment extends OnsitePayment
                     throw new ValidationException($result);
                 }
             }
-            
+
             // NOTE: Stripe will validate clientside and if for some reason that falls through
             // it will fail on payment and give an error then. It would be a lot of work to get
             // the token to be namespaced so it could be passed here and there would be no point.
---
+++ src/Extensions/PaymentIntentPurchaseService.php
@@ -0,0 +1,58 @@
+<?php
+
+
+namespace Innoweb\SilvershopStripe\Extensions;
+
+
+use Omnipay\Common\Message\RequestInterface;
+use Omnipay\Common\Message\ResponseInterface;
+use SilverStripe\Core\Extension;
+
+class PaymentIntentPurchaseService extends Extension
+{
+    /**
+     * Adds required data
+     *
+     * @param array $data
+     */
+    public function onBeforePurchase(array &$data)
+    {
+        $payment = $this->owner->getPayment();
+
+        /** @var Order $order */
+        $order = $payment->Order();
+
+        if ($payment->Gateway === 'Stripe_PaymentIntents') {
+            $data['paymentMethod'] = $data['token'];
+            unset($data['token']);
+            $data['confirm'] = true;
+        }
+
+
+    }
+
+    public function onAfterSendPurchase(RequestInterface $request,ResponseInterface $response)
+    {
+        $payment = $this->owner->getPayment();
+
+        if ($response instanceof \Omnipay\Stripe\Message\PaymentIntents\Response) {
+            // Store the Payment Intent reference for later...
+            $payment->StripePaymentIntentReference = $response->getPaymentIntentReference();
+            $payment->write();
+        }
+    }
+
+    public function onBeforeCompletePurchase(array &$data = [])
+    {
+        // Hack to get the payment, as silverstripe-omnipay doesn't currently
+        // provide a getPayment() method in PaymentService
+        $reflectionProperty = new ReflectionProperty(get_class($this->owner), 'payment');
+        $reflectionProperty->setAccessible(true);
+
+        $payment = $reflectionProperty->getValue($this->owner);
+        if ($payment->StripePaymentIntentReference) {
+            // Pass the Payment Intent reference with the transaction data to Stripe
+            $data['paymentIntentReference'] = $payment->StripePaymentIntentReference;
+        }
+    }
+}
